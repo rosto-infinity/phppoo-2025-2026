7c365d47d5cd1b027a0f8f9072cc15c137a8bb3f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-10-29 23:23:55 +0100

    -Elle représente une caractéristique ou un état de l’objet.

--- 02-proprietes-methodes-objets-php.php
+++ 02-proprietes-methodes-objets-php.php
@@ -2,7 +2,7 @@
 
 // Qu’est-ce qu’une propriété ?
 //--Une propriété (ou attribut) est une variable déclarée à l’intérieur d’une classe.
-//Elle représente une caractéristique ou un état de l’objet.
+//-Elle représente une caractéristique ou un état de l’objet.
 class Personne
 {
     public string $prenom;
