32e8dc6ab77b6c4e9741e436d9c0be0accc153b3
ROSTAND LELE <rostoinfocus@gmail.com>
2025-10-29 23:23:33 +0100

    Qu’est-ce qu’une propriété ?

---
+++ 02-proprietes-methodes-objets-php.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Qu’est-ce qu’une propriété ?
+//-Une propriété (ou attribut) est une variable déclarée à l’intérieur d’une classe.
+//Elle représente une caractéristique ou un état de l’objet.
+class Personne
+{
+    public string $prenom;
+    public int $age;
+    public string $metier;
+}
+
