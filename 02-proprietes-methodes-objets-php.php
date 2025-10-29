472e8102ae7a5d04a7e7212e0a69afa998e1986e
ROSTAND LELE <rostoinfocus@gmail.com>
2025-10-29 23:24:31 +0100

    -Méthode qui retourne une chaîne

--- 02-proprietes-methodes-objets-php.php
+++ 02-proprietes-methodes-objets-php.php
@@ -8,5 +8,22 @@ class Personne
     public string $prenom;
     public int $age;
     public string $metier;
+
+    // -Méthode qui retourne une chaîne
+    public function sePresenter(): string
+    {
+        return "Bonjour, je m'appelle {$this->prenom}, j'ai {$this->age} ans.";
+    }
+
+    // Méthode qui modifie l'état de l'objet
+    public function feterAnniversaire(): void
+    {
+        $this->age++;
+    }
 }
+
+$diane = new Personne();
+$diane->prenom = "Diane";
+$diane->age = 28;
+$diane->metier = "développeuse";
 
